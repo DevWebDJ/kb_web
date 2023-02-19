375169912ffced11b28439b205bf536f29ba6648
AiHKhaled <aithammouk40@gmail.com>
2023-02-19 17:06:47 +0100

    feat(component): contact us form for the website

--- pages/api/transporter.js
+++ pages/api/transporter.js
@@ -1,20 +1,19 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
-const username = process.env.CPANEL_WEBMAIL_USERNAME;
-const password = process.env.CPANEL_WEBMAIL_PASSWORD;
+const username = process.env.CPANEL_WEBMAIL_USERNAME
+const password = process.env.CPANEL_WEBMAIL_PASSWORD
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.HOST,
+  host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
     user: username,
     pass: password,
   },
-});
+})
 
 export const mailOptions = {
   from: username,
   to: username,
-};
-
+}
