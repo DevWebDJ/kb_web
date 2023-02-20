0e208d6fb1ccd480332deb746637f0f07321061b
DevWebDJ <djawed.m.mazari@gmail.com>
2023-02-20 08:16:29 +0100

    devis working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,40 +1,43 @@
-import { mailOptions, transporter } from './transporter'
+import { mailOptions, transporter } from "./transporter";
 
 const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    const data = req.body
+  if (req.method === "POST") {
+    const data = req.body;
+    const test = `${data.fullName}`;
     const message = `
-  fullName: ${data.fullName}\r\n
-  Email: ${data.email}\r\n
- Phone: ${data.phone}\r\n
-  Secteur: ${data.secteur}\r\n
- Activity: ${data.activity}\r\n,
-    Besoin: ${data.besoin}\r\n,
-`
+  Nom & Prénom : ${data.fullName}\r\n
+  Nom de l'entreprise : ${data.entreprise}\r\n
+  Adresse Mail : ${data.email}\r\n
+  Numéro de téléphone : ${data.phone}\r\n
+  Besoins : ${data.secteur}\r\n
+  Secteur d'activité : ${data.activity}\r\n
+  Besoin exprimé : ${data.besoin}\r\n
+`;
     if (
       !data.fullName ||
+      !data.entreprise ||
       !data.email ||
       !data.phone ||
       !data.secteur ||
       !data.activity ||
       !data.besoin
     ) {
-      return res.status(400).json({ message: 'Bad request' })
+      return res.status(400).json({ message: "Bad request" });
     }
     try {
       await transporter.sendMail({
         ...mailOptions,
-        subject: 'Nouveau Rendez-Vous!',
+        subject: `Demande de devis - ${test}`,
         text: message,
-        html: message.replace(/\r\n/g, '<br>'),
-      })
-      return res.status(200).json({ success: true })
+        html: message.replace(/\r\n/g, "<br>"),
+      });
+      return res.status(200).json({ success: true });
     } catch (error) {
-      console.log(error)
-      return res.status(400).json({ message: error.message })
+      console.log(error);
+      return res.status(400).json({ message: error.message });
     }
   }
-  return res.status(400).json({ message: 'Bad request' })
-}
+  return res.status(400).json({ message: "Bad request" });
+};
 
-export default handler
+export default handler;
--- pages/devis.js
+++ pages/devis.js
@@ -1,107 +1,160 @@
-import React, { Fragment, useState } from 'react'
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
-import Layout from '../components/Layout'
-import { services } from '../data/constant'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import Lottie from 'lottie-react'
-import waving from '../public/assets/waving.json'
-import e from 'cors'
+import React, { Fragment, useState } from "react";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
+import Layout from "../components/Layout";
+import { services } from "../data/constant";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import Lottie from "lottie-react";
+import waving from "../public/assets/waving.json";
+import e from "cors";
+import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+
+const navigation = {
+  main: [
+    { name: "Nos Logiciels", href: "/software" },
+    { name: "Services", href: "/services/" },
+    { name: "Qui sommes-nous ?", href: "/about" },
+  ],
+  social: [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/profile.php?id=100057631592030",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/kb.developpement",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: "Twitter",
+      href: "https://twitter.com/DeveloppementKb",
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+        </svg>
+      ),
+    },
+    {
+      name: "Linkedin",
+      href: "https://www.linkedin.com/company/kb-d%C3%A9veloppement/",
+      icon: FaLinkedin,
+    },
+  ],
+};
 
 const sector = [
   { id: 0, name: `Selectionnez votre secteur d'activité` },
-  { id: 1, name: 'Clinique / Cabinet Médical' },
-  { id: 2, name: 'Construction BTP' },
-  { id: 3, name: 'Commerce / Distribution' },
-  { id: 4, name: 'Immobilier' },
-  { id: 5, name: 'Énergie, Mines, Matière première' },
-  { id: 6, name: 'Industries' },
-  { id: 7, name: 'Informatique/Télécom' },
-  { id: 8, name: 'Service publics / Administrations' },
-  { id: 9, name: 'Services & Prestations' },
-  { id: 10, name: 'Fonction Libérale' },
-  { id: 11, name: 'Agroalimentaire' },
-  { id: 12, name: 'Hôtellerie & Restauration ' },
-  { id: 13, name: 'Autre' },
-]
+  { id: 1, name: "Clinique / Cabinet Médical" },
+  { id: 2, name: "Construction BTP" },
+  { id: 3, name: "Commerce / Distribution" },
+  { id: 4, name: "Immobilier" },
+  { id: 5, name: "Énergie, Mines, Matière première" },
+  { id: 6, name: "Industries" },
+  { id: 7, name: "Informatique/Télécom" },
+  { id: 8, name: "Service publics / Administrations" },
+  { id: 9, name: "Services & Prestations" },
+  { id: 10, name: "Fonction Libérale" },
+  { id: 11, name: "Agroalimentaire" },
+  { id: 12, name: "Hôtellerie & Restauration " },
+  { id: 13, name: "Autre" },
+];
 const offices = [
   {
     id: 1,
-    city: 'Oran',
+    city: "Oran",
     address: ["15 Boulevard Ibn Dirham l'Hippodrome, Oran, Algérie, 31000"],
   },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const Devis = () => {
-  const [selected, setSelected] = useState(sector[0])
+  const [selected, setSelected] = useState(sector[0]);
   const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    phone: '',
+    fullName: "",
+    entreprise: "",
+    email: "",
+    phone: "",
     secteur: [],
     activity: selected,
-    besoin: '',
-  })
+    besoin: "",
+  });
   React.useEffect(() => {
     setFormData({
       ...formData,
       activity: selected.name,
-    })
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selected])
+  }, [selected]);
 
   function handleChange(event) {
-    const { name, checked, value, type } = event.target
-    if (type === 'checkbox') {
+    const { name, checked, value, type } = event.target;
+    if (type === "checkbox") {
       if (checked) {
         setFormData({
           ...formData,
           secteur: [...formData.secteur, name],
-        })
+        });
       } else {
         setFormData({
           ...formData,
           secteur: formData.secteur.filter((item) => item !== name),
-        })
+        });
       }
     } else {
       setFormData({
         ...formData,
         [name]: value,
-      })
+      });
     }
   }
 
   const formSubmission = async () => {
     fetch(`http://localhost:3000/api/contact`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(formData),
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-    })
-  }
+    });
+  };
   const handleSubmit = async () => {
-    await formSubmission()
-    console.log(formData)
-  }
+    await formSubmission();
+    console.log(formData);
+  };
 
   const handleMenuChange = (id, type) => (e) => {
     if (e.target.checked) {
       setMenu((prev) => {
-        return { ...prev, [type]: [...prev[type], id] }
-      })
+        return { ...prev, [type]: [...prev[type], id] };
+      });
     } else {
       setMenu((prev) => {
-        return { ...prev, [type]: prev[type].filter((item) => item !== id) }
-      })
+        return { ...prev, [type]: prev[type].filter((item) => item !== id) };
+      });
     }
-  }
+  };
 
   return (
     <>
@@ -298,12 +351,8 @@ const Devis = () => {
                         </svg>
                       </div>
                       <h3 className="text-lg font-medium text-white">
-                        Contact information
+                        Contactez-nous
                       </h3>
-                      <p className="mt-6 max-w-3xl text-base text-blue-50">
-                        Nullam risus blandit ac aliquam justo ipsum. Quam mauris
-                        volutpat massa dictumst amet. Sapien tortor lacus arcu.
-                      </p>
                       <dl className="mt-8 space-y-6">
                         <dt>
                           <span className="sr-only">Contactez-nous</span>
@@ -328,62 +377,22 @@ const Devis = () => {
                         </dd>
                       </dl>
                       <ul role="list" className="mt-8 flex space-x-12">
-                        <li>
-                          <a
-                            className="text-blue-200 hover:text-blue-100"
-                            href="#"
-                          >
-                            <span className="sr-only">Facebook</span>
-                            <svg
-                              className="h-7 w-7"
-                              aria-hidden="true"
-                              fill="currentColor"
-                              viewBox="0 0 24 24"
+                        {navigation.social.map((item) => (
+                          <li>
+                            <Link
+                              target="_blank"
+                              key={item.name}
+                              href={item.href}
+                              className="text-blue-200 hover:text-white"
                             >
-                              <path
-                                fillRule="evenodd"
-                                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                                clipRule="evenodd"
+                              <span className="sr-only">{item.name}</span>
+                              <item.icon
+                                className="h-6 w-6"
+                                aria-hidden="true"
                               />
-                            </svg>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="text-blue-200 hover:text-blue-100"
-                            href="#"
-                          >
-                            <span className="sr-only">Instagram</span>
-                            <svg
-                              className="h-7 w-7"
-                              aria-hidden="true"
-                              fill="currentColor"
-                              viewBox="0 0 24 24"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="text-blue-200 hover:text-blue-100"
-                            href="#"
-                          >
-                            <span className="sr-only">Twitter</span>
-                            <svg
-                              className="h-7 w-7"
-                              aria-hidden="true"
-                              fill="currentColor"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                          </a>
-                        </li>
+                            </Link>
+                          </li>
+                        ))}
                       </ul>
                     </div>
 
@@ -414,6 +423,25 @@ const Devis = () => {
                               autoComplete="name"
                               className="block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                               required
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="entreprise"
+                            className="block text-sm font-medium text-gray-900"
+                          >
+                            Entreprise
+                          </label>
+                          <div className="mt-1">
+                            <input
+                              type="text"
+                              name="entreprise"
+                              id="entreprise"
+                              value={formData.entreprise}
+                              onChange={handleChange}
+                              autoComplete="entreprise"
+                              className="block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                             />
                           </div>
                         </div>
@@ -510,7 +538,7 @@ const Devis = () => {
                                   </label>
                                 </div>
                               </div>
-                            )
+                            );
                           })}
                         </fieldset>
                         <div className="">
@@ -551,9 +579,9 @@ const Devis = () => {
                                             className={({ active }) =>
                                               classNames(
                                                 active
-                                                  ? 'text-white bg-blue-600'
-                                                  : 'text-gray-900',
-                                                'relative cursor-default select-none py-2 pl-8 pr-4'
+                                                  ? "text-white bg-blue-600"
+                                                  : "text-gray-900",
+                                                "relative cursor-default select-none py-2 pl-8 pr-4"
                                               )
                                             }
                                             value={person}
@@ -563,9 +591,9 @@ const Devis = () => {
                                                 <span
                                                   className={classNames(
                                                     selected
-                                                      ? 'font-semibold'
-                                                      : 'font-normal',
-                                                    'block truncate'
+                                                      ? "font-semibold"
+                                                      : "font-normal",
+                                                    "block truncate"
                                                   )}
                                                 >
                                                   {person.name}
@@ -575,9 +603,9 @@ const Devis = () => {
                                                   <span
                                                     className={classNames(
                                                       active
-                                                        ? 'text-white'
-                                                        : 'text-blue-600',
-                                                      'absolute inset-y-0 left-0 flex items-center pl-1.5'
+                                                        ? "text-white"
+                                                        : "text-blue-600",
+                                                      "absolute inset-y-0 left-0 flex items-center pl-1.5"
                                                     )}
                                                   >
                                                     <CheckIcon
@@ -621,7 +649,7 @@ const Devis = () => {
                               maxLength={1500}
                               className="block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-primaryBlue focus:ring-blue-500"
                               aria-describedby="message-max"
-                              defaultValue={''}
+                              defaultValue={""}
                               value={formData.besoin}
                               onChange={handleChange}
                             />
@@ -679,7 +707,7 @@ const Devis = () => {
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Devis
+export default Devis;
